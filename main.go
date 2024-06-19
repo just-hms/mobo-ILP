9ab6d0c2b222d0cf09aa18a3db9d41ec125a5134
just-hms <alessandroversari1@gmail.com>
2024-06-19 23:23:43 +0200

    add: test with assertion

--- main.go
+++ main.go
@@ -3,37 +3,44 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 
 	"github.com/just-hms/mobo/pkg/mobo"
 	"github.com/just-hms/mobo/pkg/opt"
 )
 
 func main() {
-	for nOuts := range 2 {
-		for nIn := range 2 {
-			for k := range 10 {
-				onesRatio := float64(k+1) / 10
+	test := 10
+	for tt := range test {
+		for nOuts := range 10 {
+			for nIn := range 10 {
+				for onesRatio := 0.1; onesRatio < 0.50+1e-3; onesRatio += 0.1 {
 
-				outs := mobo.TestOutputs(nOuts, nIn, onesRatio, rand.Int())
+					seed := rand.Int()
+					outs := mobo.TestOutputs(nOuts, nIn, onesRatio, seed)
 
-				ports, _, globalCost := mobo.Solve(outs)
+					start := time.Now()
+					ports, _, globalCost := mobo.Solve(outs)
+					duration := time.Since(start)
 
-				err := mobo.Assert(outs, ports)
-				if err != nil {
-					panic(err)
-				}
-
-				totalCost := 0.0
-				for _, out := range outs {
-					ports, _, cost := mobo.Solve([]*opt.Output{out})
 					err := mobo.Assert(outs, ports)
 					if err != nil {
-						panic(err)
+						panic(fmt.Errorf("err: %s, seed: %d", err, seed))
 					}
-					totalCost += cost
-				}
 
-				fmt.Printf("%d,%d,%.2f,%.2f\n", nOuts, nIn, globalCost, totalCost)
+					totalCost := 0.0
+					for outIdx, out := range outs {
+						singleOut := []*opt.Output{out}
+						ports, _, cost := mobo.Solve(singleOut)
+						err := mobo.Assert(singleOut, ports)
+						if err != nil {
+							panic(fmt.Errorf("err: %s, seed: %d, out: %d", err, seed, outIdx))
+						}
+						totalCost += cost
+					}
+
+					fmt.Printf("%d,%d,%d,%d,%.2f,%.2f,%.2f,%d\n", tt, seed, nOuts, nIn, onesRatio, globalCost, totalCost, duration.Milliseconds())
+				}
 			}
 		}
 	}
