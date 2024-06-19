a9e5cf50c3f6f49a7da73afd98aee76b4610b531
just-hms <alessandroversari1@gmail.com>
2024-06-19 23:50:08 +0200

    add: benchmarks

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"math/rand"
 	"time"
@@ -10,27 +11,43 @@ import (
 )
 
 func main() {
-	test := 10
-	for tt := range test {
-		for nOuts := range 10 {
-			for nIn := range 10 {
+	tests := 10
+	for range tests {
+		for nOuts := 1; nOuts <= 8; nOuts++ {
+			for nIn := 1; nIn <= 8; nIn++ {
 				for onesRatio := 0.1; onesRatio < 0.50+1e-3; onesRatio += 0.1 {
 
 					seed := rand.Int()
 					outs := mobo.TestOutputs(nOuts, nIn, onesRatio, seed)
 
-					start := time.Now()
-					_, _, globalCost := mobo.Solve(outs)
-					duration := time.Since(start)
+					ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
+					defer cancel()
 
-					totalCost := 0.0
-					for _, out := range outs {
-						singleOut := []*opt.Output{out}
-						_, _, cost := mobo.Solve(singleOut)
-						totalCost += cost
+					done := make(chan struct{})
+
+					go func() {
+
+						start := time.Now()
+						_, _, globalCost := mobo.Solve(outs)
+						duration := time.Since(start)
+
+						totalCost := 0.0
+						for _, out := range outs {
+							singleOut := []*opt.Output{out}
+							_, _, cost := mobo.Solve(singleOut)
+							totalCost += cost
+						}
+
+						fmt.Printf("%d,%d,%d,%.2f,%.2f,%.2f,%d\n", seed, nOuts, nIn, onesRatio, globalCost, totalCost, duration.Milliseconds())
+						done <- struct{}{}
+					}()
+
+					select {
+					case <-done:
+					case <-ctx.Done():
+						fmt.Printf("%d,%d,%d,%.2f,%d,%d,timed out\n", seed, nOuts, nIn, onesRatio, -1, -1)
 					}
 
-					fmt.Printf("%d,%d,%d,%d,%.2f,%.2f,%.2f,%d\n", tt, seed, nOuts, nIn, onesRatio, globalCost, totalCost, duration.Milliseconds())
 				}
 			}
 		}
